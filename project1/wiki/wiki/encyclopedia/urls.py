4de2160eb9f1751a94ec3b9a5c816dba1ad05c39
Samuca47Prog <samuelsimao47@gmail.com>
2022-09-01 23:04:43 -0300

    tentando resolver o form

--- project1/wiki/wiki/encyclopedia/urls.py
+++ project1/wiki/wiki/encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:name>", views.renderPage, name="renderPage")
--- project1/wiki/wiki/encyclopedia/views.py
+++ project1/wiki/wiki/encyclopedia/views.py
@@ -1,16 +1,29 @@
+from logging import PlaceHolder
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
 import markdown
 
+class Query(forms.Form):
+    q = forms.CharField(label="Search..")
 
 def index(request):
+    if request.method == "POST":
+        form = Query(request.POST)
+        name = form.cleaned_data["q"]
+        return renderPage(request, name)
+    
+    entries = util.list_entries()
+    entries.remove("notFound")
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries,
+        "form": Query()
     })
 
-def renderPage(request, name):
+def renderPage(request, name): 
+    name = name.lower()
     entries = list(map(lambda x: x.lower(), util.list_entries()))
     if name not in entries:
         name = "notFound"
