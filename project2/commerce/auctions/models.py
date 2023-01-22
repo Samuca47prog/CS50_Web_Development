a230b8a58debee8d4e02ea6ae29b3fcc50e9d89c
Samuca47Prog <samuelsimao47@gmail.com>
2023-01-22 17:33:39 -0300

    finish v1

--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     pass
 
 class Categories(models.Model):
-    name = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64, unique=True, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
